0e414a2661df9a258b76388976857ca451de053c
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-14 19:00:25 +0100

    added install requires get dependencies from requirements.txt file!

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ import os
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 # Setting up
 setup(
     name='cbc',
-    version='0.0.1',
+    version='0.0.3',
     author='nemo256 (Amine Neggazi)',
     author_email='<neggazimedlamine@gmail.com>',
     description='Count Blood Cells',
@@ -16,7 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/nemo256/cbc',
-    install_requires=['opencv-python', 'tensorflow', 'tensorflow_addons', 'scipy', 'numpy'],
+    install_requires=required,
     keywords=['python', 'artificial intelligence', 'deep learning', 'blood cells', 'image segmentation', 'unet'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -24,6 +27,16 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
+    ],
+    data_files=[
+        ('share/cbc/weights', [
+            'models/rbc.h5',
+            'models/wbc.h5',
+            'models/plt.h5'
+        ]),
+        ('share/cbc/images', [
+            'test.jpg',
+        ])
     ],
     scripts=['cbc']
 )
