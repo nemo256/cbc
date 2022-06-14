ebf272a3aa0ee029f8404c37ff00006bb685d1e9
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-14 23:14:26 +0100

    updated to the new release version 1.0.0 stable!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 # setting up
 setup(
     name='cbc',
-    version='0.0.9',
+    version='1.0.0',
     author='nemo256 (Amine Neggazi)',
     author_email='<neggazimedlamine@gmail.com>',
     description='Count Blood Cells',
