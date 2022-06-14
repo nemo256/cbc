74e41f0f8d0242eb222950d0d2a290522022e719
Amine Neggazi <neggazimedlamine@gmail.com>
2022-06-15 00:15:46 +0100

    fixed arg parser defaults when it overrides necessary values!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 # setting up
 setup(
     name='cbc',
-    version='1.0.3',
+    version='1.0.4',
     author='nemo256 (Amine Neggazi)',
     author_email='<neggazimedlamine@gmail.com>',
     description='Count Blood Cells',
